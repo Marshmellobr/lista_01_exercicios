f8ef5a394301fbdfd0d2f4beda2582935cea04ef
JOAO VITOR DE CARVALHO <joao.vcarvalho7@senacsp.edu.br>
2025-01-16 14:48:22 -0300

    first commit

---
+++ Lista01/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*Verificação de paridade Objetivo: 
+ * Pratique o uso do operador % (módulo) e estruturas condicionais simples.
+Descrição: Crie um programa que solicite que o usuário digite um número inteiro. O programa deve verificar se o número é par ou ímpar. Use a condição if para determinar o resultado e exibir uma mensagem apropriada. 
+Exemplo de saída: "O número é par." ou "O número é ímpar".*/
+
+Console.WriteLine("Digite um numero");
+int num1 = int.Parse (Console.ReadLine());
+
+if (num1 % 2 == 0)
+{
+    Console.WriteLine("O numero é par");
+}
+
+else if (num1 % 2 == 1)
+{
+    Console.WriteLine("O numero é impar");
+}
---
+++ lista02/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Número Positivo ou Negativo
+ 
+
+Objetivo: Aprenda a trabalhar com várias condições. 
+Descrição: Desenvolva um programa que solicite ao usuário um número. O programa deve determinar se o número é positivo, negativo ou zero, usando if, else if e else para todas as três possibilidades. Exiba a mensagem correspondente. 
+Exemplo de saída: "O número é positivo.", "O número é negativo." ou "O número é zero". */
+
+Console.WriteLine("Digite um numero");
+int num1 = int.Parse(Console.ReadLine());
+
+if (num1 >= 1)
+{
+    Console.WriteLine("O numero é Positivo");
+}
+
+else if (num1  <= -1)
+{
+    Console.WriteLine("O numero é Negativo");
+}
+else if (num1 == 0)
+{
+    Console.WriteLine("O numero é zero");
+}
+
